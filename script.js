a23658bf7cb12a39e818373b3d6908c82ad783f6
Evgeeso <evhenii.soroka@gmail.com>
2024-12-16 23:54:30 +0200

    More modern aproach

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 import $ from "jquery";
 
-// or alternatively:
-$(document).ready(function() {
-    $('.list-item:first').hover(function () { 
-        $(this).toggleClass();
+document.addEventListener('DOMContentLoaded', function() {
+    $('.list-item:first')
+        .on('mouseenter mouseleave', function() {
+            $(this).toggleClass('active');
+        });
+
+    $('.list-item:eq(2)').on('click', function() {
+        $('.image:even').fadeToggle('slow');
     });
 
-    $('.list-item:eq(2)').on('click', () => {
-        $('.image:even').fadeToggle('slow');
-    })
-
-    $('.list-item:eq(4)').on('click', () => {
+    $('.list-item:eq(4)').on('click', function() {
         $('.image:odd').animate(
             {
                 opacity: 'toggle',
                 height: 'toggle'
-            }, 2000 )
-    })
-
+            }, 2000 );
+    });
 });
