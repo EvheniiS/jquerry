b3de231a84f00d95bb5a33e6dcc1e30dec809c93
Evgeeso <evhenii.soroka@gmail.com>
2024-12-16 23:51:07 +0200

    basics | depricated

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-import "jquery";
+import $ from "jquery";
 
+// or alternatively:
+$(document).ready(function() {
+    $('.list-item:first').hover(function () { 
+        $(this).toggleClass();
+    });
+
+    $('.list-item:eq(2)').on('click', () => {
+        $('.image:even').fadeToggle('slow');
+    })
+
+    $('.list-item:eq(4)').on('click', () => {
+        $('.image:odd').animate(
+            {
+                opacity: 'toggle',
+                height: 'toggle'
+            }, 2000 )
+    })
+
+});
